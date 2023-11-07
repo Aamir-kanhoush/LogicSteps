866819ff6bb875bf5843f50fe28f36a3358ef86a
“Aamir-kanhoush” <aamir.kanhoush2001@gmail,com>
2023-11-07 09:30:37 -0500

    Fixed Rules

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -6,13 +6,16 @@ import static Game.Move.moveLocation;
 
 public class Main {
     public static void main(String[] args) {
-        //GameLoop();
+//        GameLoop();
         State state = new State(new Grid());
         state.grid.loadLevel("D:\\Intellij projects\\LogicSteps\\src\\Game\\level.txt");
-        System.out.println(state);
-        UCS.searchUCS(state, 10);
-
-        //BFS.searchBFS(state,100);
+        System.out.println("start:");
+        state.grid.printGrid();
+        state.grid.printLocations();
+        System.out.println("Game on:");
+//        UCS.searchUCS(state, 2000000000);
+//        BFS.searchBFS(state,2000000000);
+//        DFS.searchDFS(state,2000000000);
 //        Set<State> nextState = State.getNextState(state);
 //        for (State obj:nextState) {
 //            obj.grid.printLocations();
@@ -31,7 +34,7 @@ public class Main {
 
     public static void GameLoop() {
         State state = new State(new Grid());
-        state.grid.loadLevel("D:\\Intellij projects\\LogicSteps\\src\\Game\\level.txt");
+        state.grid.loadLevel("D:\\Intellij projects\\LogicSteps\\src\\Game\\level2.txt");
         Scanner scanner = new Scanner(System.in);
         int positionIndex, direction;
         state.grid.printGrid();
@@ -47,16 +50,19 @@ public class Main {
             if(Move.isValidPosition(positionIndex,state)){
                 System.out.println("Enter the direction to move (8 for up, 6 for right, 4 for left, 2 for down):");
                 direction = scanner.nextInt();
-                state=moveLocation(positionIndex,direction,state);
-
+                if(Move.isValidMove(positionIndex,direction,state)){
+                    state=Move.moveLocation(positionIndex,direction,state);
                     state.grid.printGrid();
                     state.grid.printLocations();
-
+                }else {
+                    System.out.println("Invalid move.");
+                }
             }else {
                 System.out.println("Bad index");
             }
         }
         System.out.println("YOU WIN!!!\nHURRAY!!!");
     }
+
 
 }
